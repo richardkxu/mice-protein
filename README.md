fc9d84e3c2c5c0a00e4cf02abb7c12fcd8c17ee1
richardkxu <richardkxu@gmail.com>
2019-04-25 11:04:32 -0500

    finish svm model with cross valid

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mice-protein
+## data
+* create a ./data directory and put all .csv files in this folder
--- svm.py
+++ svm.py
@@ -1,41 +1,46 @@
 import os, sys
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
 
 
 class SVMClassifier:
     def __init__(self, X, y, kerenl_type='linear', test_ratio=0.25):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=test_ratio, random_state=0)
-        self.clf = SVC(kernel=kerenl_type)
+        self.kernel_type = kerenl_type
 
     def train_test(self):
         Cs = np.logspace(-6, -1, 10)
-        best_clf = GridSearchCV(estimator=self.clf, param_grid=dict(C=Cs), n_jobs = -1)
-        best_clf.fit(self.X_train, self.y_train)
-        print("Best score: %0.2f".format(best_clf.best_score_))
-        print("Best para: {}".format(best_clf.best_params_))
-        best_clf.score(self.X_test, self.y_test)
+        self.best_clf = GridSearchCV(estimator=SVC(kernel=self.kernel_type), param_grid=dict(C=Cs), n_jobs = -1)
+        self.best_clf.fit(self.X_train, self.y_train)
+        print("Best training score: {0.3f}".format(self.best_clf.best_score_))
+        print("Best para: {}".format(self.best_clf.best_params_))
+        test_score = self.best_clf.score(self.X_test, self.y_test)
+        print("Test performance: {0.3f}".format(test_score))
 
-    def visualize_feat_importance(self, classifier, feature_names, top_features=20):
-        """
-        https://medium.com/@aneesha/visualising-top-features-in-linear-svm-with-scikit-learn-and-matplotlib-3454ab18a14d
-        :param classifier: 
-        :param feature_names: 
-        :param top_features: 
-        :return: 
-        """"""
-        coef = classifier.coef_.ravel()
+    def visualize_feat_importance(self, feature_names, top_features=20):
+        coef = self.best_clf.coef_.ravel()
         top_positive_coefficients = np.argsort(coef)[-top_features:]
         top_negative_coefficients = np.argsort(coef)[:top_features]
         top_coefficients = np.hstack([top_negative_coefficients, top_positive_coefficients])
         # create plot
         plt.figure(figsize=(15, 5))
-        colors = [‘red’ if c < 0 else ‘blue’ for c in coef[top_coefficients]]
+        colors = []
+        for c in coef[top_coefficients]:
+            if c <0:
+                colors.append('red')
+            else:
+                colors.append('blue')
+
         plt.bar(np.arange(2 * top_features), coef[top_coefficients], color=colors)
         feature_names = np.array(feature_names)
         plt.xticks(np.arange(1, 1 + 2 * top_features), feature_names[top_coefficients], rotation=60, ha=’right’)
         plt.show()
+
+
+if __name__ == '__main__':
+    svm_clf = SVMClassifier(X, y)
+    svm_clf.train_test()
+    svm_clf.visualize_feat_importance(feature_names, top_features=20)
