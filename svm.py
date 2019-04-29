940ea5f635208cc48f0c3f2490aea92a9fcae2ee
richardkxu <richardkxu@gmail.com>
2019-04-29 14:09:48 -0500

    refine figures, add accuracy printout

--- svm.py
+++ svm.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import pandas as pd
 
 
@@ -14,7 +15,7 @@ class SVMClassifier:
 
     def train_test(self):
         Cs = np.logspace(-6, -1, 10)
-        grid_search = GridSearchCV(estimator=SVC(kernel=self.kernel_type), param_grid=dict(C=Cs), n_jobs = -1)
+        grid_search = GridSearchCV(estimator=SVC(kernel=self.kernel_type), cv=10, param_grid=dict(C=Cs), n_jobs = -1)
         grid_search.fit(self.X_train, self.y_train)
         print("Best training score: {:0.3f}".format(grid_search.best_score_))
         print("Best para: {}".format(grid_search.best_params_))
@@ -22,27 +23,31 @@ class SVMClassifier:
         print("Test performance: {:0.3f}".format(test_score))
         self.clf = grid_search.best_estimator_
 
-    def visualize_feat_importance(self, feature_names, i, top_features=20):
+    def visualize_feat_importance(self, feature_names, kind, top_features=10):
         coef = self.clf.coef_.ravel()
         top_positive_coefficients = np.argsort(coef)[-top_features:]
         top_negative_coefficients = np.argsort(coef)[:top_features]
         top_coefficients = np.hstack([top_negative_coefficients, top_positive_coefficients])
         # create plot
         plt.figure(figsize=(16, 9))
+        plt.title("Top {} positive and negative features to predict {}".format(top_features, kind))
+
+        blue_patch = mpatches.Patch(color='blue', label='Negative features')
+        red_patch = mpatches.Patch(color='red', label='Positive features')
+        plt.legend(handles=[blue_patch, red_patch])
         colors = []
         for c in coef[top_coefficients]:
-            if c <0:
-                colors.append('red')
+            if c < 0:
+                colors.append('blue')  # top negative features
             else:
-                colors.append('blue')
+                colors.append('red')  # top positive features
 
         plt.bar(np.arange(2 * top_features), coef[top_coefficients], color=colors)
         feature_names = np.array(feature_names)
         plt.xticks(np.arange(1, 1 + 2 * top_features), feature_names[top_coefficients], rotation=60, ha='right')
-        fig_name = 'feat-importance' + str(i) + '.pdf'
+        plt.tight_layout()
+        fig_name = './results/feat-importance-' + kind + '.pdf'
         plt.savefig(fig_name)
-
-
 
 
 if __name__ == '__main__':
@@ -50,20 +55,17 @@ if __name__ == '__main__':
     kinds = ["behavior", "genotype", "treatment"]
 
     for i in range(len(kinds)):
-
+        print("\nCurrent binary classification category: {}".format(kinds[i]))
         x = pd.read_csv(base_directory + "/" + kinds[i] + "_x.csv", sep=',',header=0,index_col=0 )
         x_value = x.values
         y = pd.read_csv(base_directory + "/" + kinds[i] + "_y.csv", sep=',', header=0, index_col=0)
-        y_value = y.values
+        y_value = y.values.flatten()
         header = pd.read_csv(base_directory + "/" + kinds[i] + "_x.csv", sep=',' ,index_col=0)
         filepath = base_directory + "/" + kinds[i] + "_x.csv"
         with open(filepath) as fp:
             line = fp.readline()
             feature_names = line.split(",")[1:]
 
-
-
-
         svm_clf = SVMClassifier(x_value, y_value)
         svm_clf.train_test()
-        svm_clf.visualize_feat_importance(feature_names, i, top_features=20)
+        svm_clf.visualize_feat_importance(feature_names, kind=kinds[i])
